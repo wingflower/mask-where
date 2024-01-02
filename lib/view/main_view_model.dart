5846c7fb6fe00e30aa99508db4cd4eafaaa0046e
Dongho Gwak <wing.flower.k@gmail.com>
2024-01-02 22:53:43 +0900

    fix: ViewModel 캡슐화 적용 완료

--- lib/view/main_view_model.dart
+++ lib/view/main_view_model.dart
@@ -4,19 +4,27 @@ import '../data/model/store.dart';
 import '../data/repository/store_repository.dart';
 
 class MainViewModel extends ChangeNotifier {
-  final repository = StoreRepository();
-  List<Store> stores = [];
-  bool isLoading = false;
+  final _repository = StoreRepository();
+
+  get repository => _repository;
+
+  List<Store> _stores = [];
+
+  List<Store> get stores => _stores;
+
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
 
   MainViewModel();
 
   Future<void> getStores() async {
-    isLoading = true;
+    _isLoading = true;
     notifyListeners();
 
-    stores = await repository.fetch();
+    _stores = await repository.fetch();
 
-    isLoading = false;
+    _isLoading = false;
     notifyListeners();
   }
 }
